3550947d1e9ef1e6b9444eb39af49e11da249590
Zhou Yi <pollyjoe2003@gmail.com>
2022-04-28 10:41:56 +0800

    started to design game body

--- src/Game.h
+++ src/Game.h
@@ -9,5 +9,19 @@
 #define Game_h
 
 #include <stdio.h>
+#include "Card.h"
+#include "Player.h"
+
+/*************************************************************************
+ Process of one game :
+    1. Dealing the card: After dealing, there should be 4 cards in each player's hand
+    2. Game begins:
+        (1) Player 1 take one card from the stack, choose a card and play;
+        (2) Other players take turns to play a card if available;
+        (3) 
+*******************************************************************************/
+
+
+
 
 #endif /* Game_h */
